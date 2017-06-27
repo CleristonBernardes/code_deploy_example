aea5f500b81a5a1485a12c7ee955510a4a9cb80c
Cleriston <CleristonBernardes@users.noreply.github.com>
2017-06-27 15:57:31 +1000

    Update pos_deploy.sh

--- scripts/pos_deploy.sh
+++ scripts/pos_deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+PATH="/home/ubuntu/web/sites/altadis-codedeploy/"
+cd $PATH
 BASEDIR=$(dirname $0)
 echo "Script location: ${BASEDIR}"
+echo "Application Name: $APPLICATION_NAME"
+echo "Group Name: $DEPLOYMENT_GROUP_NAME"
 echo "pos deploy script"
 mkdir -p pos_deploy_ok
