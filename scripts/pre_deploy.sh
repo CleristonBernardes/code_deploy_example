0f35350ece8032e8ae4477763cf0ead8545db876
Cleriston <CleristonBernardes@users.noreply.github.com>
2017-06-27 16:07:41 +1000

    Update pre_deploy.sh

--- scripts/pre_deploy.sh
+++ scripts/pre_deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 PATH="/home/ubuntu/web/sites/altadis-codedeploy/"
 cd $PATH
-BASEDIR=$(dirname $0)
-echo "Script location: ${BASEDIR}"
+pwd
 echo "Application Name: $APPLICATION_NAME"
 echo "Group Name: $DEPLOYMENT_GROUP_NAME"
 echo "running pre deploy"
